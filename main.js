0ad4ea9953f26d0d15389e2e9e5c2102c83f9480
Steve Maya <steve.maya@codeimmersives.com>
2020-10-04 17:56:59 -0500

    finished challenge 3

---
+++ main.js
@@ -0,0 +1,28 @@
+function challenge1 () {
+    let num = 1;
+    while (num <= 5) {
+        console.log(num);
+        num = num + 1;
+    }
+}
+challenge1();
+
+function challenge2 (n) {
+    let num = 1;
+    while (num <= n) {
+    console.log(num);
+    num++;
+    }
+    }
+challenge2(6);
+challenge2(3);
+
+function challenge3(num1, num2) {
+    while (num1 <= num2) {
+        console.log(num1);
+        num1++;
+       
+    }
+}
+challenge3(3, 8);
+challenge3(-2, 1);
